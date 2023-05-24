847d9b7d9a84441538c8ed97cbb0a8a3b1e957a8
Eduardo Cavazin <eduardotorcav@gmail.com>
2023-05-23 21:33:39 -0300

    Início Space Invaders

---
+++ src/ifpr/paranavai/jogo/principal/Principal.java
@@ -0,0 +1,18 @@
+package ifpr.paranavai.jogo.principal;
+
+import javax.swing.JFrame;
+
+public class Principal extends JFrame {
+
+    public Principal(){
+        super.setTitle("Sapce Invaders - Duba Version");
+        super.setSize(800, 600);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setLocationRelativeTo(null);
+        super.setResizable(false);
+        super.setVisible(true);
+    }
+    
+
+    
+}
