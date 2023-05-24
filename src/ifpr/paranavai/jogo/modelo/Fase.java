d039b7e8be7b67966a61609023e6b2b1d4b2e51f
Eduardo Cavazin <eduardotorcav@gmail.com>
2023-05-23 22:51:21 -0300

    Imagem de Fundo

---
+++ src/ifpr/paranavai/jogo/modelo/Fase.java
@@ -0,0 +1,15 @@
+package ifpr.paranavai.jogo.modelo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    
+    private Image imgFundo;
+
+    public Fase(){
+        ImageIcon carregadno = new ImageIcon("C:\\Users\\Duba\\Desktop\\Testes\\POO\\Jogo\\Recursos\\loading.jpg");
+
+
+    }
+}
